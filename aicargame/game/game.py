46ced7008b283320b8b21258bfea6fbee352eee6
xShyQx <bartosz.kar@wp.pl>
2022-08-19 16:49:10 +0200

    Started adding simple collision

--- aicargame/game/game.py
+++ aicargame/game/game.py
@@ -16,6 +16,7 @@ from aicargame.game.textures.textures import Textures
 
 class Game:
     sprites = pygame.sprite.Group()
+    enemySprites = pygame.sprite.Group()
     window: pygame.Surface
     bg = Textures.BACKGROUND
     bg = pygame.transform.scale(bg, (WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -24,18 +25,26 @@ class Game:
         self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("AICarGame")
 
-        obj = Player(PLAYER_START, PLAYER_SIZE)
+        self.obj = Player(PLAYER_START, PLAYER_SIZE)
 
-        self.sprites.add(obj)
+        self.sprites.add(self.obj)
+
+    def checkCollisions(self):
+        if pygame.sprite.spritecollide(self.obj, self.enemySprites, False) != []:
+            print("Uuu kraksa")
 
     def update(self):
         cur_time = time.time()
 
         if cur_time - Enemy.timer >= ENEMY_INTERVAL:
-            self.sprites.add(Enemy.spawnEnemy())
+            self.enemySprites.add(Enemy.spawnEnemy())
             Enemy.timer = cur_time
+
+        self.checkCollisions()
 
         self.window.fill((0, 0, 0))
         self.window.blit(self.bg, (0, 0))
         self.sprites.update()
         self.sprites.draw(self.window)
+        self.enemySprites.update()
+        self.enemySprites.draw(self.window)
